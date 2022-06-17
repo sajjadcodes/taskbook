86b868b137542b944fd00f66889f2a2b0adcc849
sajjadcodes <sakraan01@gmail.com>
2022-06-17 09:25:18 +0500

    add: registered CPT named task in the plugin

--- includes/posttypes.php
+++ includes/posttypes.php
@@ -2,10 +2,65 @@
 /**
  * Register a new "Task" post type.
  *
- * Called from taskbook.php.
+ * Called from tasktask.php.
  * 
- * @package  Taskbook
+ * @package  Tasktask
  * @link     https://developer.wordpress.org/plugins/post-types/registering-custom-post-types/
  */
 
 
+/**
+ * Register a custom post type called "task".
+ *
+ * @see get_post_type_labels() for label keys.
+ */
+function taskbook_cpt_init() {
+    $labels = array(
+        'name'                  => _x( 'Tasks', 'Post type general name', 'textdomain' ),
+        'singular_name'         => _x( 'Task', 'Post type singular name', 'textdomain' ),
+        'menu_name'             => _x( 'Tasks', 'Admin Menu text', 'textdomain' ),
+        'name_admin_bar'        => _x( 'Task', 'Add New on Toolbar', 'textdomain' ),
+        'add_new'               => __( 'Add New', 'textdomain' ),
+        'add_new_item'          => __( 'Add New Task', 'textdomain' ),
+        'new_item'              => __( 'New Task', 'textdomain' ),
+        'edit_item'             => __( 'Edit Task', 'textdomain' ),
+        'view_item'             => __( 'View Task', 'textdomain' ),
+        'all_items'             => __( 'All Tasks', 'textdomain' ),
+        'search_items'          => __( 'Search Tasks', 'textdomain' ),
+        'parent_item_colon'     => __( 'Parent Tasks:', 'textdomain' ),
+        'not_found'             => __( 'No tasks found.', 'textdomain' ),
+        'not_found_in_trash'    => __( 'No tasks found in Trash.', 'textdomain' ),
+        'featured_image'        => _x( 'Task Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+        'archives'              => _x( 'Task archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+        'insert_into_item'      => _x( 'Insert into task', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this task', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+        'filter_items_list'     => _x( 'Filter tasks list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+        'items_list_navigation' => _x( 'Tasks list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+        'items_list'            => _x( 'Tasks list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+    );
+ 
+    $args = array(
+        'labels'             => $labels,
+        'public'             => false,
+        'publicly_queryable' => false,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'tasks' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'menu_icon'          => 'dashicons-excerpt-view',
+        'show_in_rest'       =>true,
+        'rest_base'          =>'tasks',
+        'supports'           => array( 'title', 'editor', 'author'),
+    );
+ 
+    register_post_type( 'task', $args );
+}
+ 
+add_action( 'init', 'taskbook_cpt_init' );
