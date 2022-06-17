93d691c13fa24294a4a02ed13e8849cec19bfc16
sajjadcodes <sakraan01@gmail.com>
2022-06-17 09:30:13 +0500

    add: added rewrite rules on custom post type init with plugin activation

--- includes/posttypes.php
+++ includes/posttypes.php
@@ -64,3 +64,15 @@ function taskbook_cpt_init() {
 }
  
 add_action( 'init', 'taskbook_cpt_init' );
+
+function taskbook_rewrite_flush() {
+    // First, we "add" the custom post type via the above written function.
+    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
+    // They are only referenced in the post_type column with a post entry, 
+    // when you add a post of this CPT.
+    taskbook_cpt_init();
+ 
+    // ATTENTION: This is *only* done during plugin activation hook in this example!
+    // You should *NEVER EVER* do this on every page load!!
+    flush_rewrite_rules();
+}
